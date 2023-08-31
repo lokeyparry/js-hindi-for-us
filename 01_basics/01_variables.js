1888c9d90bcc8467b0f935f6766988da0547a6be
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2023-08-31 20:39:09 +0000

    lokey

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountid=1444
+let myName="Rockey"
+var email="loakey@gmail.com"
+User="fattu"
+//  accountid=666
+myName="Parvej"
+email="parry@gmail.com"
+// console.log(User);
+// console.log(accountid);
+// console.log(email);
+// console.log(myName);
+
+// for output all in table
+console.table([accountid, myName, email, User])
