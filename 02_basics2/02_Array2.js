50a8b3d224c015b28a965e78c2f50302d2b38e27
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2023-09-04 23:40:47 +0000

    ko

---
+++ 02_basics2/02_Array2.js
@@ -0,0 +1,17 @@
+const mrHeros=["Thor","Falcon","spiderman"]
+const dcHeros=["superman","flash","batman"]
+
+// mrHeros.push(dcHeros)
+// console.log(mrHeros)
+
+
+// const allHeros=mrHeros.concat(dcHeros)
+// console.log(allHeros)
+
+// const allHeros=[...mrHeros,...dcHeros]
+// console.log(allHeros)
+
+const newArray=[1,2,3,[2,3,4],5,6[,3,4,5,[4,5]]]
+const oldArray=newArray.flat(infinite)
+
+
