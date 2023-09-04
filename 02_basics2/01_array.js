c09abd80d763a3737054b8db4a7018c5454a7405
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2023-09-04 23:13:52 +0000

    jo

---
+++ 02_basics2/01_array.js
@@ -0,0 +1,20 @@
+// arrays
+const myArr=[0,2,3,4,5,6]
+const myHeros=['saktiman','puspa','bahubali']
+const myArr2=new Array(1,2,3,4)
+console.log(myArr2);
+// myArr.push(8)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.indexOf(8));
+// if value not found in array it gives -1
+
+
+console.log("A ",myArr);
+const myn1=myArr.slice(1,4,2)
+console.log(myn1)
+console.log("B ", myArr)
+const myn2=myArr.splice(1,4)
+console.log("C ", myArr)
+console.log(myn2)
